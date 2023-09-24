4bc1c08855d42d0d3b49d394224957019345d8da
motabbirhossain <rajuahmed362002@gmail.com>
2023-09-24 17:03:56 +0600

    text project

---
+++ assets/js/active.js
@@ -0,0 +1,27 @@
+/*
+    Template: Rafter - Roofing Services HTML5 Template
+    Author: ThemeJunction
+*/
+
+(function ($) {
+	"use strict";
+
+	$(document).ready(function () {
+		/*==========================
+				Owl Carousel Init
+		============================*/
+			
+		/* ==========================
+				#wow  init
+		=============================*/
+		if ($(".wow").length) {
+			var wow = new WOW({
+				boxClass: "wow",
+				animateClass: "animated",
+				mobile: true,
+				live: true,
+			});
+			wow.init();
+		}
+	});
+})(jQuery); // End jQuery
